093e51ad27b7332746efa3149ad25d5cd63b7649
lakshmi-p15 <lakshmi.pavuluri15@gmail.com>
2023-07-25 14:13:53 -0700

    App now displays maps with Singapore pointed with marker. Satellite view setup with Zoom options.

--- app/src/main/java/com/example/mapsjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/mapsjetpackcompose/MainActivity.kt
@@ -1,16 +1,35 @@
 package com.example.mapsjetpackcompose
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
 import com.example.mapsjetpackcompose.ui.theme.MapsJetpackComposeTheme
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.CameraPositionState
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapType
+import com.google.maps.android.compose.MapUiSettings
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +41,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val singapore = LatLng(1.35, 103.87)
+                    val cameraPositionState = rememberCameraPositionState{
+                        position = CameraPosition.fromLatLngZoom(singapore, 10f)
+                    }
+                    MyGoogleMap(singapore, cameraPositionState)
                 }
             }
         }
@@ -30,17 +53,33 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MyGoogleMap(
+    currentLocation: LatLng,
+    cameraPositionState: CameraPositionState,
+) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MapsJetpackComposeTheme {
-        Greeting("Android")
+    val uiSettings by remember {
+        mutableStateOf(MapUiSettings())
+    }
+    val properties by remember {
+        mutableStateOf(MapProperties(mapType = MapType.SATELLITE))
+    }
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        GoogleMap(
+            modifier = Modifier.fillMaxSize(),
+            cameraPositionState = cameraPositionState,
+            properties = properties,
+            uiSettings = uiSettings
+        ) {
+            Marker(
+                state = MarkerState(position = currentLocation),
+                title = "Singapore",
+                snippet = "Marker in Singapore"
+            )
+        }
     }
 }
+
+
+
